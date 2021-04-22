74a53ea8fd0f9569089aab16dd2234607f8e536a
Christophe Boulanger <christophe.boulanger@imio.be>
2021-04-22 11:32:25 +0200

    Override some plone translations

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-domain=imio.smartweb
 locales_directory=src/imio/smartweb/locales/locales
 
+domain=imio.smartweb
 bin/i18ndude rebuild-pot --pot $locales_directory/$domain.pot --create $domain devel/*
 bin/i18ndude sync --pot $locales_directory/$domain.pot $locales_directory/*/LC_MESSAGES/$domain.po
+
+domain=plone
+bin/i18ndude sync --pot $locales_directory/$domain-manual.pot $locales_directory/*/LC_MESSAGES/$domain.po
